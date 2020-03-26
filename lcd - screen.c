d5b8e9967078ee6a7acfbddfa7f17d03087293fa
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2020-03-25 23:51:16 -0600

    Modificacion en los tiempos del barrido

--- lcd - screen.c
+++ lcd - screen.c
@@ -19,7 +19,7 @@ void main() {
          printf(lcd_putc, "Julio Cesar");                     //Manda a imprimir el nombre "Julio Cesar"
          lcd_gotoxy(a,2);                                     //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Jimenez Bolio");                   //Manda a imprimir el apellido "Jimenez Bolio"
-         delay_ms(500);                                       //Tiene un retraso de 100 milisegundos
+         delay_ms(250);                                       //Tiene un retraso de 100 milisegundos
          a = a - 1;                                           //Decrementanos la variable para cambiar la posicion de la impresion en la pantalla
          if (a == 0) {
             delay_ms(2000);
@@ -36,7 +36,7 @@ void main() {
          printf(lcd_putc, "Victor Manuel");                   //Manda a imprimir el nombre "Victor Manuel"
          lcd_gotoxy(a,2);                                     //Mueve el apuntador interno a la 2da fila y decrementa conforme a la variable en la columna
          printf(lcd_putc, "Sanchez Sanchez");                 //Manda a imprimir el apellido "Sanchez Sanchez"
-         delay_ms(500);                                       //Tiene un retraso de 100 milisegundos
+         delay_ms(350);                                       //Tiene un retraso de 100 milisegundos
          a = a - 1;                                           //Decrementanos la variable para cambiar la posicion de la impresion en la pantalla
          if (a == 0) {
             delay_ms(2000);
